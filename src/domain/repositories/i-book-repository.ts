1303b41009f6736018c8613c5643a18c5bb33a01
Rafet Mercimek <106338097+rafetmer@users.noreply.github.com>
2025-08-21 16:29:01 +0300

    fix(book): input ve return typelardaki hatalar giderildi

--- src/domain/repositories/i-book-repository.ts
+++ src/domain/repositories/i-book-repository.ts
@@ -3,9 +3,9 @@ import { IGenericRepository } from './i-generic.repository.js';
 
 export interface IBookRepository  extends IGenericRepository<Book>{
 
-    findBooksByAuthorName(author: string): Promise<String[]>; 
+    findBooksByAuthor(author: string): Promise<Book[]>; 
 
     create(data: Omit<Book, 'id' | 'createdAt' | 'updatedAt'>): Promise<Book>;
     
-    update(id: number, data:Omit<Book, 'id'>): Promise<Book>;
+    update(id: number, data:Partial<Omit<Book, 'id'>>): Promise<Book>;
 };
