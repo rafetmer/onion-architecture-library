1f881c6e6c2aab98a1beb5bda3f3ac357bf52e9c
Rafet Mercimek <106338097+rafetmer@users.noreply.github.com>
2025-08-21 16:39:20 +0300

    feat(user): findUserByEmail function added

--- src/domain/repositories/i-user-repository.ts
+++ src/domain/repositories/i-user-repository.ts
@@ -3,9 +3,9 @@ import { IGenericRepository } from './i-generic.repository.js';
 
 export interface IUserRepository extends IGenericRepository<User> {
 
-    findBooksByAuthorName(author: string): Promise<String[]>;
-
     create(data: Omit<User, 'id' | 'createdAt' | 'updatedAt'>): Promise<User>;
 
     update(id: number, data: Partial<Omit<User, 'id'>>): Promise<User>;
+
+    findUserByEmail(emial: string): Promise<User>;
 }
