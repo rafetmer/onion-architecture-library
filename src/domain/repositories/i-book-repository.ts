67806e2119dc62cda09a179ad6a7344687d3d0fc
Rafet Mercimek <106338097+rafetmer@users.noreply.github.com>
2025-08-22 11:44:50 +0300

    feat(book): introduce new function findAvailableBooks

--- src/domain/repositories/i-book-repository.ts
+++ src/domain/repositories/i-book-repository.ts
@@ -4,6 +4,8 @@ import { IGenericRepository } from './i-generic.repository.js';
 export interface IBookRepository  extends IGenericRepository<Book>{
 
     findBooksByAuthor(author: string): Promise<Book[]>; 
+    
+    findAvailableBooks(): Promise<Book[]>;
 
     create(data: Omit<Book, 'id' | 'createdAt' | 'updatedAt'>): Promise<Book>;
     
